ed455ca4bf207df1f815bf3447f013befa32cb00
Lautidev10 <Lautarodevincenzi@hotmail.com>
2026-06-05 14:02:49 -0300

    Reinicio intentos

--- Servicio/MonitorAcceso_82CD.cs
+++ Servicio/MonitorAcceso_82CD.cs
@@ -17,24 +17,24 @@ namespace Servicio
         private static readonly string rutaArchivo_82CD = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "IntentosFallidos_82CD.json");
         
         
-        private static Dictionary<string, int> CargarDiccionarioIntentos_82CD()
+        private static Dictionary<string, ServicioIntentosUsuario_82CD> CargarDiccionarioIntentos_82CD()
         {
             if (!File.Exists(rutaArchivo_82CD))
-                return new Dictionary<string, int>();
+                return new Dictionary<string, ServicioIntentosUsuario_82CD>();
 
             string json_82CD = File.ReadAllText(rutaArchivo_82CD);
 
-            Dictionary <string, int> intentos_82CD = JsonConvert.DeserializeObject<Dictionary<string, int>>(json_82CD);
+            Dictionary <string, ServicioIntentosUsuario_82CD> intentos_82CD = JsonConvert.DeserializeObject<Dictionary<string, ServicioIntentosUsuario_82CD>>(json_82CD);
 
             if(intentos_82CD == null)
             {
-                return new Dictionary<string, int>();
+                return new Dictionary<string, ServicioIntentosUsuario_82CD>();
             }
 
             return intentos_82CD;
         }
 
-        private static void GuardarDiccionarioIntentos_82CD(Dictionary<string, int> intentos_82CD)
+        private static void GuardarDiccionarioIntentos_82CD(Dictionary<string, ServicioIntentosUsuario_82CD> intentos_82CD)
         {
             string json_82CD = JsonConvert.SerializeObject(intentos_82CD,Formatting.Indented);
             File.WriteAllText(rutaArchivo_82CD, json_82CD);
@@ -42,29 +42,43 @@ namespace Servicio
 
         public static int RegistrarIntentoFallido_82CD(string login_82CD)
         {
-            Dictionary<string, int> intentos_82CD = CargarDiccionarioIntentos_82CD();
+            Dictionary<string, ServicioIntentosUsuario_82CD> intentos_82CD = CargarDiccionarioIntentos_82CD();
 
             if (!intentos_82CD.ContainsKey(login_82CD))
             {
-                intentos_82CD[login_82CD] = 0;
+                intentos_82CD[login_82CD] = new ServicioIntentosUsuario_82CD
+                {
+                    CantidadIntentos_82CD = 0,
+                    FechaUltimoIntento_82CD = DateTime.Now
+                };
             }
 
-            intentos_82CD[login_82CD]++;
+            ServicioIntentosUsuario_82CD intentosUsuario_82CD = intentos_82CD[login_82CD];
+
+            // se reinicia el contador si pasaron mas de 5 horas
+            if ((DateTime.Now - intentosUsuario_82CD.FechaUltimoIntento_82CD).TotalHours >= 5)
+            {
+                intentosUsuario_82CD.CantidadIntentos_82CD = 0;
+            }
+
+            intentosUsuario_82CD.CantidadIntentos_82CD++;
+            intentosUsuario_82CD.FechaUltimoIntento_82CD = DateTime.Now;
+
             GuardarDiccionarioIntentos_82CD(intentos_82CD);
 
-            return intentos_82CD[login_82CD];
+            return intentosUsuario_82CD.CantidadIntentos_82CD;
         }
 
         public static int ObtenerIntentos_82CD(string login_82CD)
         {
-            Dictionary<string, int> diccionarioIntentos_82CD = CargarDiccionarioIntentos_82CD();
+            Dictionary<string, ServicioIntentosUsuario_82CD> diccionarioIntentos_82CD = CargarDiccionarioIntentos_82CD();
 
             if (!diccionarioIntentos_82CD.ContainsKey(login_82CD))
             {
                 return 0;
             }
             
-            return diccionarioIntentos_82CD[login_82CD];
+            return diccionarioIntentos_82CD[login_82CD].CantidadIntentos_82CD;
         }
 
         public static bool Bloquear(string login_82CD)
@@ -75,7 +89,7 @@ namespace Servicio
 
         public static void EliminarIntentos_82CD(string login_82CD)
         {
-            Dictionary<string, int> diccionarioIntentos_82CD = CargarDiccionarioIntentos_82CD();
+            Dictionary<string, ServicioIntentosUsuario_82CD> diccionarioIntentos_82CD = CargarDiccionarioIntentos_82CD();
 
             if (diccionarioIntentos_82CD.ContainsKey(login_82CD))
             {
---
+++ Servicio/ServicioIntentosUsuario_82CD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicio
+{
+    public class ServicioIntentosUsuario_82CD
+    {
+        public int CantidadIntentos_82CD { get; set; }
+        public DateTime FechaUltimoIntento_82CD { get; set; }
+    }
+}
